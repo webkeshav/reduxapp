610dd36a95ebef6ea5feab6e389eebfba8b2b278
Keshav <keshavtomar999@gmail.com>
2022-07-06 16:28:21 +0530

    First commit for redux strore

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+      <h3 className='m-4'>Buy this Adidas Shoes 50 Rs</h3>
+      <button className='btn btn-primary mx-3'>-</button>
+      Add this to cart
+      <button className='btn btn-primary mx-3'>+</button>
+    </div>
+  )
+}
+
+export default Shop
